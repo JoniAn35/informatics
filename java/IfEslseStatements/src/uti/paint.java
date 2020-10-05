6e5c78b567fd7ab06e99dcba7630577ebf3e3220
JoniAn35 <72023404+JoniAn35@users.noreply.github.com>
2020-10-05 15:47:06 +0300

    arrays + few projects

---
+++ java/IfEslseStatements/src/uti/paint.java
@@ -0,0 +1,29 @@
+package uti;
+
+import java.util.Scanner;
+
+public class paint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double height = sc.nextDouble();
+		double lenght = sc.nextDouble();
+		double width = sc.nextDouble();
+		
+		double squareMeters = height * lenght * 2 + height * width * 2
+				+ width * lenght;
+		
+		double bucket1L , bucket5L;
+		bucket5L = Math.ceil(squareMeters / 140);
+		bucket1L = Math.ceil(squareMeters / 30);
+		
+		if(bucket5L * 15 > bucket1L * 4){
+			System.out.println("You need " + (int)bucket1L + " 1-liter bucket/s");
+		}
+		else {
+			System.out.println("You need " + (int)bucket5L + " 5-liter bucket/s");
+		}
+	}
+
+}
