b90b25f767863bb36e46c90e77f86f205553d21e
JoniAn35 <72023404+JoniAn35@users.noreply.github.com>
2020-10-08 17:26:03 +0300

    edited isItAnAnagram

--- java/Arrays/src/uti/onePositionBackwards.java
+++ java/Arrays/src/uti/onePositionBackwards.java
@@ -19,14 +19,16 @@ public class onePositionBackwards {
 		}
 		
 		int x = nums[0];
-		for(int i = 1; i < 10; i++) {
-			nums[i - 1] = nums[i];
+		for(int i = 1; 1 < 10; i++) {
+			if(i == 9) {
+				nums[i] = x;
+			}
+			else {
+				nums[i - 1] = nums[i];
+			}
 		}
-		nums[9] = x;
 		
-		for (int i = 0; i < 10; i++) {
-			System.out.print(nums[i] + " ");
-		}
+		
 	}
 
 }
