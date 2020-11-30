8fd37de049e0e1db8774cdd34d757bfd8c4ce8e3
Yoana Angelova <yoana.an@gmail.com>
2020-11-30 15:53:12 +0200

    added Plymorphism

---
+++ java/Polymorphism/src/uti/ChildrenBook.java
@@ -0,0 +1,14 @@
+package uti;
+
+public class ChildrenBook extends Book {
+	int age;
+	
+	public ChildrenBook(double regularPrice, String publisher, int yearPublished, int age) {
+		super(regularPrice, publisher, yearPublished);
+		this.age = age;
+	}
+	
+	public double computeSalePrice(){
+        return super.getRegularPrice() * 0.3;			//overriding
+    }
+}
