777644f29bbb32231d1c359b07272701ec78c923
Yoana Angelova <yoana.an@gmail.com>
2020-12-04 18:25:20 +0200

    added project AbtractClasses

---
+++ java/AbstractClasses/src/uti/ComicBook.java
@@ -0,0 +1,14 @@
+package uti;
+
+public class ComicBook extends Book{
+	String characterName;
+	
+	public ComicBook(double regularPrice, String publisher, int yearPublished, String characterName) {
+		super(regularPrice, publisher, yearPublished);
+		this.characterName = characterName;
+	}
+	
+	public double computeSalePrice(){
+        return super.getRegularPrice() * 0.4;			//overriding
+    }
+}
